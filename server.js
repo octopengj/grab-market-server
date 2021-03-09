310712104c19c73783374c3f7f7773bb43b25086
octopengj <octopengj@google.com>
2021-03-09 20:50:36 +0900

    상품 생성 & 조회 기능 추가

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/products", (req, res) => {
+  models.Product.findAll()
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("에러 발생");
+    });
+});
+
+app.post("/products", (req, res) => {
+  const body = req.body;
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("모든 필드를 입력해주세요");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 업로드에 문제가 발생했습니다");
+    });
+});
+
+app.get("/products/:id/events/:eventId", (req, res) => {
+  const params = req.params;
+  const { id, eventId } = params;
+  res.send(`id는 ${id}와 ${eventId}입니다`);
+});
+
+app.listen(port, () => {
+  console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB 연결 에러ㅠ");
+      process.exit();
+    });
+});
